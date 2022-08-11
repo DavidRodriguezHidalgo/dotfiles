7d6430b87ddd2b2c952f69c093534977efd3b8f6
David Rodriguez <denzilrh90@gmail.com>
2022-08-11 13:12:14 +0200

    auto-session

--- dot_config/nvim/lua/denzil/packer.lua
+++ dot_config/nvim/lua/denzil/packer.lua
@@ -41,4 +41,14 @@ return require('packer').startup(function(use)
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-buffer'
   use 'folke/which-key.nvim'
+  -- Sessions
+  use {
+  'rmagatti/auto-session',
+    config = function()
+      require("auto-session").setup {
+       log_level = "error",
+       auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+      }
+    end
+  }
 end)
