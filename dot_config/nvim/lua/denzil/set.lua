cb902910601419251e7ab7eb8b4fd4511ed8b5f5
David Rodriguez <denzilrh90@gmail.com>
2022-09-04 11:16:16 +0200

    move sets to lua

--- dot_config/nvim/lua/denzil/set.lua
+++ dot_config/nvim/lua/denzil/set.lua
@@ -1,38 +1,37 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.numberwidth = 1
+vim.opt.clipboard='unnamed' 
+vim.opt.showcmd = true
+vim.opt.ruler = true
+vim.opt.encoding = 'utf-8'
+vim.opt.showmatch = true
+vim.opt.sw = 2
+vim.opt.laststatus = 2
+vim.opt.showmode = false
+vim.opt.autoindent = true
+vim.opt.lazyredraw = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.swapfile = false
+vim.opt.hidden = true
+vim.opt.digraph = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.smartcase = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.cursorline = true
+vim.opt.ttyfast = true
 vim.cmd([[
-set number
-set mouse=a
-set numberwidth=1
-set clipboard=unnamed 
-set showcmd
-set ruler
-set encoding=utf-8
-set showmatch
-set sw=2
-
-set laststatus=2
-set noshowmode
-set autoindent
-set lazyredraw
-set nohlsearch
-set incsearch
-set noswapfile
-set hidden
-set digraph
 set list listchars=tab:··,trail:·
-set tabstop=2
-set shiftwidth=2
-set expandtab
-set smartindent
-set smartcase
-set termguicolors
-set scrolloff=8
 set signcolumn=yes
-"set completeopt=menuone,noselect
 set completeopt=noinsert,menuone,noselect " Modifies the auto-complete menu to behave more like an IDE.
-set cursorline " Highlights the current line in the editor
 
 set cc=120 " Show at 120 column a border for good code style
-set ttyfast " Speed up scrolling in Vim
 ]])
 
 -- Use the space key as our leader.
